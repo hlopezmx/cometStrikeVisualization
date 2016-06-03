cd40eead032b1ee0c063a4a9e093f9773e074217
Hugo Lopez <hlt@live.com.mx>
2016-06-03 13:54:20 +0100

    Add files via upload

---
+++ js/meteorites.js
@@ -0,0 +1,192 @@
+var alldata = [],           // loaded data from CSV
+    workingdata = [],       // filtered data
+    currentZoom = 1,
+    initialYear = 0,
+    finalYear = 0,
+    yearX,
+    yearDelay;
+
+// using d3, we select the html element with "map" id and append an svg tag
+// where d3 will be adding svg items to render the map
+var svg = d3.select('#map')
+    .append('svg')
+    .attr('id', 'svg')
+    .attr('width', setting.svgWidth)
+    .attr('height', setting.svgHeight)
+
+// get the width from the screen (so remove the 'px' at the end)
+setting.mapWidth = svg.style("width").replace('px', '');
+// calculate the height based on the weight, preserverving the ratio (make it an int)
+setting.mapHeight = parseInt(parseFloat(setting.mapWidth) * parseFloat(setting.mapImageRatio));
+
+
+// set the svg size to use the whole browser view
+svg
+    .attr('width', setting.mapWidth)
+    .attr('height', setting.mapHeight)
+    .attr("xmlns", "http://www.w3.org/2000/svg")
+
+// this method will let us project from longitude and latitude to pixel coordinates
+var projection = d3.geo.equirectangular()
+    .scale(setting.projectionScale)
+    .translate([setting.mapWidth / 2, setting.mapHeight / 2])
+
+// a path to append the worldmap polygons to
+var path = d3.geo.path()
+    .projection(projection);
+
+// definiton of the zoom and pan behavior
+var zoom = d3.behavior.zoom()
+    .on("zoom", function () {
+
+        // store the current scale, it will be useful
+        // to repaint some elements without scaling them
+        // e.g. map contours, meteorite points.
+        currentZoom = zoom.scale();
+
+        // zoom and pan
+        g.attr("transform", "translate(" +
+            d3.event.translate.join(",") + ")scale(" + d3.event.scale + ")");
+
+        // restore the worldmap contours width, so they don't
+        // get wider when zooming in
+        g.selectAll("path")
+            .style("stroke-width", function (d) {
+                var nStrokeWidth = $(this).css("stroke-width").replace("px", "");
+                nStrokeWidth = nStrokeWidth != 0 ? 1 : nStrokeWidth;
+                return nStrokeWidth / currentZoom + "px";
+            })
+
+        // restore the meteorites' width, so they don't
+        // get wider when zooming in
+        g.selectAll("circle")
+            .attr('r', 3 / currentZoom)
+
+    });
+svg.call(zoom)
+
+// create an element group where we will be adding items
+var g = svg.append("g")
+    .call(zoom);
+
+// load the json file with the map polygons and add css styling
+d3.json("data/worldmap.json", function (error, world) {
+    if (error) throw error;
+
+    // append a group with "countries" id and set the data from the json info
+    // also set the style (using css attributes)
+    g.append("g")
+        .attr("id", "countries")
+        .selectAll("path")
+        .data(topojson.feature(world, world.objects.countries).features)
+        .enter().append("path")
+        .attr("d", path)
+        .style('stroke', '#8a7842')
+        .style('fill', '#c2b280')
+        .style('stroke-width', '1px')
+
+    // load the meteorites data
+    loadData();
+});
+
+// --- load the data set
+function loadData() {
+    workingdata = [];
+    d3.csv(setting.dataFile, function (data) {
+        data = data.map(initItem);
+        for (key in data) alldata.push(data[key])
+        alldata.sort(compareYear);
+        workingdata = alldata.slice(0);
+        initialYear = parseInt(workingdata[0].YEAR);
+        finalYear = parseInt(workingdata[workingdata.length - 1].YEAR);
+
+        yearX = d3.scale.linear()
+            .domain([initialYear, finalYear])
+            .range([100, setting.mapWidth])
+
+        yearDelay = d3.scale.linear()
+            .domain([initialYear, finalYear])
+            .range([1000, 1000 + finalYear])
+
+        displayData();
+    });
+}
+
+// display the meteorites
+function displayData() {
+
+    // data label
+    var gDataInfo = g.append('g')
+        .attr('id', 'dataInfo')
+    /*
+     var rDataInfo = g.append('rect')
+     .attr('x', 10)
+     .attr('y', 280)
+     .attr('width', 250)
+     .attr('height', 300)
+     .style('fill', 'rgba(255,255,255,0.5)')
+     .style('stroke', 'rgba(125,125,125,0.5)')
+     */
+
+    var textInfo = gDataInfo.append('text')
+        .text('')
+        .attr('x', 20)
+        .attr('y', 300)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '100px')
+        .attr('font-weight', 'bold')
+        .attr('fill', 'rgba(0,0,0,0.5')
+
+
+    // Create a group for the Meteorite elements:
+    gDataPoints = g.append('g')
+        .attr('id', 'datapoints')
+
+    // add a white filled circle for each meteorite
+    gDataPoints.selectAll('circle')
+        .data(workingdata)
+        .enter()
+        .append('circle')
+        .style('fill', 'white')
+        .style('opacity', 0.3)
+        .attr('cx', function (d) {
+            // convert longitude and latitude coordinates into pixel coordinates
+            return projection([d.longitude, d.latitude])[0];
+        })
+        .attr('cy', function (d) {
+            // convert longitude and latitude coordinates into pixel coordinates
+            return projection([d.longitude, d.latitude])[1];
+        })
+        .attr('r', 0 / currentZoom)     // we start with a nil radius
+        .transition()                   // and will animate...
+        .delay(function (d, i) {        // delay each meteorite based on its row number
+            //return i * 50;
+
+            return yearDelay(i);
+        })
+        .attr('r', 3 / currentZoom)     // after the delay, increase the radius.
+        .each('end', function (d, i) {
+            textInfo.text(d.YEAR);
+            textInfo.attr('x', yearX(i));
+            //textInfo.attr('x', yearX(d.YEAR));
+        })
+}
+
+
+// Transform the data set into the structure we will be working with
+function initItem(d) {
+    d.latitude = parseFloat(d['COORDINATE']);
+    d.longitude = parseFloat(d['COORDINA_1']);
+    d.YEAR = parseInt(d['YEAR']);
+    return d;
+}
+
+// sort meteorite elements by year
+function compareYear(a, b) {
+    if (a.YEAR < b.YEAR)
+        return -1;
+    else if (a.YEAR > b.YEAR)
+        return 1;
+    else
+        return 0;
+}
--- js/settings.js
+++ js/settings.js
@@ -1,11 +1,11 @@
 // SETTINGS
 var setting = {
-    dataFile: 'data/asteroids_fell.csv'    // the source data file
-    , svgHeight: '100vh'        // SVG area height
-    , svgWidth: '100vw'         // SVG area width
-    , mapImageRatio: 0.5        // ration between width and height
-    , projectionScale: 200      // scale for projection (automatically updated when printing)
-    , mapWidth: 960             // map Width
-    , mapHeight: 480            // map Height
+    dataFile: 'data/meteorites.csv'     // the source data file
+    , svgHeight: '100vh'                // SVG area height
+    , svgWidth: '100vw'                 // SVG area width
+    , mapImageRatio: 0.5                // ration between width and height
+    , projectionScale: 200              // scale for projection (automatically updated when printing)
+    , mapWidth: 960                     // map Width
+    , mapHeight: 480                    // map Height
 }
 
